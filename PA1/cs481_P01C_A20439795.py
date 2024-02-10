8e20fe8425631a6a015b6b8530edc300f4221d1a
Jiecheng Mei <jmei18@hawk.iit.edu>
2024-02-10 12:25:06 -0600

    done?

--- PA1/cs481_P01C_A20439795.py
+++ PA1/cs481_P01C_A20439795.py
@@ -7,10 +7,13 @@ from collections import Counter
 from nltk.corpus import brown
 from nltk.corpus import stopwords
 
+
 if __name__ == '__main__':
     N = 2
     # load
-    filtered_brown = [word for word in brown.words() if word not in stopwords.words()]
+    brown = nltk.corpus.brown.words()
+    stopwords = nltk.corpus.stopwords.words()
+    filtered_brown = [word for word in brown if word not in stopwords]
     # after filtered, this is what we need to search
     brown_count = Counter(filtered_brown)
 
@@ -22,6 +25,8 @@ if __name__ == '__main__':
             match_ngrams = [ngram for ngram in ngrams_brown if ngrams_brown[0].lower == userInput]
             ngrams_count = Counter(match_ngrams)
             top_3_ngrams = ngrams_count.most_common(3)
+            top_3_ngrams.sort(key=lambda a: a[1])
+            top_3_ngrams.reverse()
             probability_top_3_ngrams = []
             for word, count in top_3_ngrams:
                 prob = count / brown_count[userInput]
@@ -31,12 +36,19 @@ if __name__ == '__main__':
                 counter += 1
                 word, _ = info[0]
                 prob = info[1]
-                print(f'{counter}. {word} | Probability = {prob}')
+                print(f'{counter}.  {word} | Probability = {prob}')
             print(f'4. quit')
-            # 做完了打印结果，缺拉出答案并且更新userinput
-
-
-
+            userChoice = input('Select one to continue:')
+            userChoice = 1 if userChoice not in [1, 2, 3, 4] else _
+            if userChoice == 1:
+                userInput = top_3_ngrams[0][0]
+            elif userChoice == 2:
+                userInput = top_3_ngrams[1][0]
+            elif userChoice == 3:
+                userInput = top_3_ngrams[2][0]
+            elif userChoice == 4:
+                sys.exit(0)
+            continue
         else:
             while True:
                 choice = input('a. Try again\n b.quit').lower()
